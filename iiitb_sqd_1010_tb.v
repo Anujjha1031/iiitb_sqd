63d190112ece58368903fe4914ecaf6260a917f0
Anujjha1031 <110462872+Anujjha1031@users.noreply.github.com>
2022-08-09 20:00:38 +0530

    Add files via upload

---
+++ iiitb_sqd_1010_tb.v
@@ -0,0 +1,26 @@
+module iiitb_sqd_1010_tb;
+reg din,clk,reset;
+wire y;
+iiitb_sqd_1010 m1(din, reset, clk, y);
+initial
+begin
+reset=0       ;clk=0;din=0;
+$monitor($time, , ,"c=%b",clk,,"y=%b",y,,"r=%b",reset,,"d=%b",din);
+#5 din=1;
+#5 din=1;
+#5 din=1;
+#10 din=0;
+#10 din=1;
+#10 din=0;
+#10 din=1;
+#10 din=0;
+#5 din=1;
+//#5 reset=1;
+//#5 reset=0;
+
+end
+always
+#5 clk=~clk;
+initial
+#80 $finish ;
+endmodule
